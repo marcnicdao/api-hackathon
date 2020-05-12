80ab65c6ddbd7ad31a9794407d8986158b04bf4c
Marc Nicdao <nicdaom46^@gmail.com>
2020-05-12 12:34:51 -0700

    In the process of adding an info modal

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 class App {
-    constructor(mainContainer, popularContainer, upcomingContainer, searchedContainer, searchField) {
-        this.baseUrl = 'https://api.themoviedb.org/3/?api_key=a135da89bb4463a852b9155a6280f76b'
+    constructor(mainContainer, popularContainer, upcomingContainer, searchedContainer, searchField, movieModal) {
         this.mainContainer = mainContainer;
         this.popularContainer = popularContainer
         this.upcomingContainer = upcomingContainer
@@ -11,76 +10,88 @@ class App {
         this.getMoviesErrorHandler = this.getMoviesErrorHandler.bind(this);
         this.getTrailerLinkSuccessHandler = this.getTrailerLinkSuccessHandler.bind(this)
         this.trailerLink = null;
+        this.movieModal = movieModal;
+        this.myApikey1 = myApikey1;
     }
     getPopularMovies() {
         $.ajax({
             method: "GET",
-            url: 'https://api.themoviedb.org/3/movie/popular?api_key=a135da89bb4463a852b9155a6280f76b&language=en-US&page=1',
+            url: `https://api.themoviedb.org/3/movie/popular?api_key=${this.myApikey1}&language=en-US&page=1`,
             success: (movies) => this.getMoviesSuccessHandler(movies.results, this.popularContainer),
             error: this.getMoviesErrorHandler
         })
     }
+
+
     getMoviesSuccessHandler(movies, container) {
-        console.log(movies)
         this.loadMovies(movies, container)
     }
 
     getMoviesErrorHandler(err) {
         console.error(err)
     }
+
     getUpcomingMovies() {
         $.ajax({
             method: "GET",
-            url: 'https://api.themoviedb.org/3/movie/upcoming?api_key=a135da89bb4463a852b9155a6280f76b&language=en-US&page=1',
+            url: `https://api.themoviedb.org/3/movie/upcoming?api_key=${this.myApikey1}&language=en-US&page=1`,
             success: (movies) => this.getMoviesSuccessHandler(movies.results, this.upcomingContainer),
             error: this.getPopularErrorHandler
         })
     }
+
     getSearchedMovies(title) {
         this.searchedContainer.textContent = " ";
         $.ajax({
             method: "GET",
-            url: `https://api.themoviedb.org/3/search/movie?api_key=a135da89bb4463a852b9155a6280f76b&append_to_response=people&query=${title}`,
+            url: `https://api.themoviedb.org/3/search/movie?api_key=${this.myApikey1}&append_to_response=people&query=${title}`,
             success: (movies) => this.getMoviesSuccessHandler(movies.results, this.searchedContainer),
             error: this.getPopularErrorHandler
         })
         this.searchField.value = " ";
     }
+
     loadMovies(movies, container) {
         movies.forEach((movie) => {
             var movieCard = document.createElement('div');
-            var anchor = document.createElement('a');
+            var trailerAnchor = document.createElement('a');
+            var moviePoster = document.createElement('img');
             //debugger
             this.getTrailerLink(movie.id);
-            anchor.dataset.movieId = movie.id
-            anchor.href = this.trailerLink
-            anchor.target = '_blank'
+
+            trailerAnchor.href = this.trailerLink
+            trailerAnchor.target = '_blank'
             movieCard.classList.add('cards')
-            var moviePoster = document.createElement('img');
             moviePoster.src = `https://image.tmdb.org/t/p/w200${movie.poster_path}`
-            movieCard.append(moviePoster);
-            console.log(moviePoster.src)
-            anchor.append(movieCard)
+            trailerAnchor.append(moviePoster);
+            movieCard.append(trailerAnchor)
+            movieCard.dataset.movieId = movie.id
+
+
             if(movie.poster_path){
-                container.append(anchor);
+                container.append(movieCard);
             }
         })
     }
+
     getTrailerLink(movieId) {
 
         $.ajax({
             method: "GET",
             async: false,
-            url: `http://api.themoviedb.org/3/movie/${movieId}/videos?api_key=a135da89bb4463a852b9155a6280f76b`,
-            success: (response) => { this.getTrailerLinkSuccessHandler(response.results) },
+            url: `http://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${this.myApikey1}`,
+            success: (response) => { this.getTrailerLinkSuccessHandler(response.results)
+            console.log(response) },
             error: this.getMoviesErrorHandler
         })
     }
 
     getTrailerLinkSuccessHandler(response) {
-        this.trailerLink = `https://www.youtube.com/watch?v=${response[0].key}`
+        if(response[0]){
+            this.trailerLink = `https://www.youtube.com/watch?v=${response[0].key}`
+
+        }
 
     }
-
 
 }
