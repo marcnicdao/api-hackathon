9455154b5beb6b968e89545f44db1c9856be18f9
Marc Nicdao <nicdaom46^@gmail.com>
2020-05-11 22:49:24 -0700

    in the process of adding trailer link

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ class App {
         this.upcomingContainer = upcomingContainer
         this.searchedContainer = searchedContainer
         this.searchField = searchField;
-        this.searchField.addEventListener('change', this.getSearchedMovies(this.searchField.value))
+        this.searchField.addEventListener('change', ()=>this.getSearchedMovies(this.searchField.value))
         this.getMoviesSuccessHandler = this.getMoviesSuccessHandler.bind(this);
         this.getMoviesErrorHandler = this.getMoviesErrorHandler.bind(this);
+        this.getTrailerLinkSuccessHandler = this.getTrailerLinkSuccessHandler.bind(this)
+        this.trailerLink = null;
     }
     getPopularMovies() {
         $.ajax({
@@ -41,16 +43,39 @@ class App {
             url: `https://api.themoviedb.org/3/search/movie?api_key=a135da89bb4463a852b9155a6280f76b&append_to_response=people&query=${title}`,
             success: (movies) => this.getMoviesSuccessHandler(movies.results, this.searchedContainer),
             error: this.getPopularErrorHandler
-        })
+        } )
+        this.searchField.value = " ";
     }
     loadMovies(movies, container) {
         movies.forEach((movie) => {
             var movieCard = document.createElement('div');
+            var anchor = document.createElement('a');
+            //debugger
+
+            this.getTrailerLink(movie.id, anchor);
+
             movieCard.classList.add('cards')
             var moviePoster = document.createElement('img');
             moviePoster.src = `https://image.tmdb.org/t/p/w200${movie.poster_path}`
             movieCard.append(moviePoster);
-            container.append(movieCard);
+            anchor.append(movieCard)
+            container.append(anchor);
         })
     }
+    getTrailerLink(movieId){
+
+        $.ajax({
+            method: "GET",
+            url: `http://api.themoviedb.org/3/movie/${movieId}/videos?api_key=a135da89bb4463a852b9155a6280f76b`,
+            success: (response)=>{this.getTrailerLinkSuccessHandler(response.results)},
+            error: this.getMoviesErrorHandler
+        })
+    }
+
+    getTrailerLinkSuccessHandler(response){
+        this.trailerLink = `www.youtube.com/watch?v=${response[0].key}`
+        anchor.href = this.trailerLink
+    }
+
+
 }
