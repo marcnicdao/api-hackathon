53427e5b4335b05c216a0d21ea2b970d1876d773
Marc Nicdao <nicdaom466@gmail.com>
2020-07-07 15:20:24 -0700

    moved api key to config js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ class App {
         this.trailerLink = null;
         this.trailerPlayer = document.querySelector('.trailer-player')
         this.movieModal = document.querySelector('.modal');
-        this.myApikey1 = 'a135da89bb4463a852b9155a6280f76b';
+        this.myApikey1 = this.myApikey1;
         this.exitModal = this.exitModal.bind(this)
         this.domElements = {}
         this.similarMovieContainer = similarMovieContainer;
