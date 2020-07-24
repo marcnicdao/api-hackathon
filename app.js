9ca694a4e559010a008b01396bb977d3f4e0afdf
Marc Nicdao <nicdaom466@gmail.com>
2020-07-24 01:38:07 -0700

    disabled scroll when modal is open

--- app.js
+++ app.js
@@ -221,6 +221,10 @@ class App {
     }
 
     showModal(e) {
+        var scrollY = window.scrollY
+        document.body.style.position = 'fixed';
+        document.body.style.width = '100vw'
+        document.body.style.top = `-${scrollY}px`;
         this.movieModal.classList.remove('hidden');
         this.getModalElements(e.target.dataset.movieId);
     }
@@ -229,6 +233,11 @@ class App {
         this.similarMovieContainer.textContent = " ";
         this.movieModal.classList.add('hidden');
         this.trailerPlayer.removeAttribute('src');
+        var scrollY = document.body.style.top;
+        console.log(scrollY)
+        document.body.style.position = '';
+        document.body.style.top = '';
+        window.scrollTo(0, parseInt(scrollY || '0') * -1);
     }
 
     eventHandler(e) {
